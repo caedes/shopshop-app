547aed0cf4a969ac1292f555b2b2285e62b96389
Romain LAURENT <laurentromain@gmail.com>
2025-09-25 15:46:41 +0200

    feat: configure queryClient with default options for query stale time

--- src/app/queryClient.ts
+++ src/app/queryClient.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "@tanstack/react-query";
+import { MINUTE_IN_MS } from "../constants";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * MINUTE_IN_MS,
+    },
+  },
+});
