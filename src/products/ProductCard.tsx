7446f5237aa968fc2b32fa8b0b64f44961c255c9
Romain LAURENT <laurentromain@gmail.com>
2025-09-30 10:24:01 +0200

    feat: refactor ProductCard to use Material-UI Card components for improved layout

--- src/products/ProductCard.tsx
+++ src/products/ProductCard.tsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router";
 import { Price } from "../ds/atoms/Price";
-import { Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 type ProductCardProps = {
   id: number;
@@ -8,13 +15,36 @@ type ProductCardProps = {
   price: number;
 };
 
+/**
+ * A simple function to get a different image for each product.
+ * Picsum has images with ids from 0 to 1084, so we add 20 to the product id
+ * to avoid having the same image as the product id.
+ *
+ * It should be avoid in production code, but it's fine for this demo app.
+ */
+const getImageId = (id: number) => {
+  return id + 20;
+};
+
 export const ProductCard = ({ id, title, price }: ProductCardProps) => {
   return (
-    <>
-      <Typography variant="h4">
-        <Link to={`/products/${id}`}>{title}</Link>
-      </Typography>
-      <Price amount={price} />
-    </>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={`https://picsum.photos/id/${getImageId(id)}/350/350`}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          <Link to={`/products/${id}`}>{title}</Link>
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          <Price amount={price} />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 };
