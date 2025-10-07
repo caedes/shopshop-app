b3a7a2cbcd8169a15b097206fc7a0f68641500ca
Romain LAURENT <laurentromain@gmail.com>
2025-10-07 11:51:00 +0200

    chore: add test:coverage script

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   server: {
     port: 3000,
   },
@@ -13,4 +13,5 @@ export default defineConfig({
       },
     }),
   ],
-});
+  test: {},
+}));
